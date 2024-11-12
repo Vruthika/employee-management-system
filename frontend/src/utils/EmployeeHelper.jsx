bc0e60bd5bd027fc32c0212c28f1fb392d571e96
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2024-11-12 09:55:52 +0530

    final commit(rounded buttons)

--- frontend/src/utils/EmployeeHelper.jsx
+++ frontend/src/utils/EmployeeHelper.jsx
@@ -88,10 +88,10 @@ export const EmployeeButtons = ({ _id }) => {
 
     return (
         <div className="flex space-x-3">
-            <button className="px-3 py-1 bg-teal-600 text-white" onClick={() => navigate(`/admin-dashboard/employees/${_id}`)}>View</button>
-            <button className="px-3 py-1 bg-blue-600 text-white" onClick={() => navigate(`/admin-dashboard/employees/edit/${_id}`)}>Edit</button>
-            <button className="px-3 py-1 bg-yellow-600 text-white" onClick={() => navigate(`/admin-dashboard/employees/salary/${_id}`)}>Salary</button>
-            <button className="px-3 py-1 bg-red-600 text-white" onClick={() => navigate(`/admin-dashboard/employees/leaves/${_id}`)}>Leave</button>
+            <button className="px-3 py-1 bg-teal-600 text-white rounded-md" onClick={() => navigate(`/admin-dashboard/employees/${_id}`)}>View</button>
+            <button className="px-3 py-1 bg-blue-600 rounded-md text-white" onClick={() => navigate(`/admin-dashboard/employees/edit/${_id}`)}>Edit</button>
+            <button className="px-3 py-1 bg-yellow-600 rounded-md text-white" onClick={() => navigate(`/admin-dashboard/employees/salary/${_id}`)}>Salary</button>
+            <button className="px-3 py-1 bg-red-600 rounded-md text-white" onClick={() => navigate(`/admin-dashboard/employees/leaves/${_id}`)}>Leave</button>
         </div>
     )
 }
